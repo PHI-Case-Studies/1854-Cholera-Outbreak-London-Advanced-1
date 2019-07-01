ee6d223aa75e58cc132aa3ec0756e870970b3795
PHI-Case-Studies <herman.tolentino@gmail.com>
2019-07-01 06:21:27 -0500

    updated azure script

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -3,15 +3,9 @@
 # Activate environment
 source /home/nbuser/anaconda3_420/bin/activate
 
-# Set up proxy
-#http_proxy=http://webproxy:3128
-#https_proxy=http://webproxy:3128
-#export http_proxy
-#export https_proxy
-
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge osmnx=0.10* folium=0.9.1
+conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* osmnx=0.10*
 
 pip install --upgrade pip
 pip install pandas==0.24.2
