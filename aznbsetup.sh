d75fba58561b7c3992d56cc4c48ce0650a177873
PHI-Case-Studies <herman.tolentino@gmail.com>
2019-07-01 09:21:07 -0500

    updated azure script

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,7 +5,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* osmnx=0.10*
+conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* osmnx=0.10* networkx=2.3*
 
 pip install --upgrade pip
 pip install pandas==0.24.2
