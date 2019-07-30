906dddf9986a3488b774e17f0992a63f16d82c49
Herman Tolentino <herman.tolentino@gmail.com>
2019-07-30 08:51:07 -0500

    updated Notebook 5

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,7 +5,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* osmnx=0.10* networkx=2.3*
+conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* networkx=2.3* shapely descartes
 
 pip install --upgrade pip
 pip install pandas==0.24.2
