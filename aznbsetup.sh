4256630e57cc1f4af4df982719214c317f62a60c
Herman Tolentino <herman.tolentino@gmail.com>
2019-08-19 23:12:11 -0500

    updated Azure script

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,8 +5,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* networkx=2.3* shapely descartes \
-    pandas=0.25*
+conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* pandas=0.25* geopandas descartes shapely poppler
 
 pip install --upgrade pip
 
