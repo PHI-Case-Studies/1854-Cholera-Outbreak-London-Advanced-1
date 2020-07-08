aa9912c4c29b75a8c14763e157f8131fbbbd9979
Herman Tolentino <herman.tolentino@gmail.com>
2020-07-08 06:49:12 -0500

    updated setup binder and azure scripts

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,7 +5,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.10* jinja2=2.10* osmnx=0.10* pandas geopandas descartes shapely poppler
+conda install -y -c conda-forge folium=0.11* jinja2=2.10* osmnx=0.15* pandas=1.0* geopandas=0.80* descartes shapely poppler
 
 pip install --upgrade pip
 
